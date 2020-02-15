d528127981e07cc8d1617d7bab3bbe570b2b8871
gkarthik <xraydiffraction@gmail.com>
2020-02-15 15:33:44 -0800

    Init

---
+++ install_R_packages.R
@@ -0,0 +1,4 @@
+install.packages(c("tidyverse", "lubridate", "caret", "randomForest", "doParallel", "glmnet"))
+install.packages("rstan", repos = "https://cloud.r-project.org/", dependencies = TRUE)
+pkgbuild::has_build_tools(debug = TRUE)
+install.packages("raster")
---
+++ packages_install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Install vim/vi/nano during install of ArchLinux
+
+# Sudo
+pacman -S sudo
+# Nvidia
+pacman -S nvidia nvidia-settings cuda
+# Intel
+pacman -S xf86-video-intel mesa
+# X
+pacman -S xorg-xrandr xorg-xinit 
+# Window manager
+pacman -S i3
+# Wifi
+pacman -S dialog wpa_supplicant wifi-menu
+# Applications
+pacman -S terminator firefox alsa-utils
+# Languages and utils
+pacman -S base-devel python python-pip r git emacs rsync openssh keepassxc tk jdk-openjdk gdb valgrind
+pip install --user numpy scipy sklearn pandas matplotlib requests biopython
+# Yay for AUR
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+# Keybase
+yay -S keybase-bin
+run_keybase
+# Install fonts
+yay -S ttf-iosevka
+# Eclipse
+yay -S eclipse-java
